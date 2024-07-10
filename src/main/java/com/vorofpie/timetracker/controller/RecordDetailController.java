6bf7f03ba555b5a340c799bfbbb90abe3c55c867
Matvey Voropay <vorofpie@gmail.com>
2024-07-10 21:00:36 +0300

    Add controllers

---
+++ src/main/java/com/vorofpie/timetracker/controller/RecordDetailController.java
@@ -0,0 +1,49 @@
+package com.vorofpie.timetracker.controller;
+
+import com.vorofpie.timetracker.dto.request.RecordDetailRequest;
+import com.vorofpie.timetracker.dto.response.RecordDetailResponse;
+import com.vorofpie.timetracker.service.RecordDetailService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/record-details")
+public class RecordDetailController {
+
+    private final RecordDetailService recordDetailService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RecordDetailResponse createRecordDetail(@RequestBody @Valid RecordDetailRequest recordDetailRequest) {
+        return recordDetailService.createRecordDetail(recordDetailRequest);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RecordDetailResponse updateRecordDetail(@PathVariable Long id, @RequestBody @Valid RecordDetailRequest recordDetailRequest) {
+        return recordDetailService.updateRecordDetail(id, recordDetailRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRecordDetail(@PathVariable Long id) {
+        recordDetailService.deleteRecordDetail(id);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RecordDetailResponse getRecordDetailById(@PathVariable Long id) {
+        return recordDetailService.getRecordDetailById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<RecordDetailResponse> getAllRecordDetails() {
+        return recordDetailService.getAllRecordDetails();
+    }
+}
